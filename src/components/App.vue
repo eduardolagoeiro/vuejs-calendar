cfd3af538975b4cbdbf8f5d120215ade5c8d1e5e
Eduardo Lagoeiro <eduardolagoeiro@id.uff.br>
2018-10-17 17:42:13 -0300

    days in a month

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,12 +1,36 @@
 <template>
-  <div> {{ msg }}</div>
+  <div>
+    <div v-for="day in days" :key="day">
+      {{day}}
+    </div>
+  </div>
 </template>
 
 <script>
   export default {
     data() {
       return {
-        msg: 'Hello World!'
+        month: 10,
+        year: 2018
+      }
+    },
+    methods: {
+      getDaysFromMonth(){
+        let days = []
+        let currentDay = this.$moment(
+          `${this.year}-${this.month}-1`,
+          'YYYY-M-D'
+        );
+        do {
+          days.push(currentDay);
+          currentDay = this.$moment(currentDay).add(1, 'days');
+        } while(currentDay.month()+1 === this.month);
+        return days;
+      }
+    },
+    computed: {
+      days(){
+        return this.getDaysFromMonth();
       }
     }
   }
